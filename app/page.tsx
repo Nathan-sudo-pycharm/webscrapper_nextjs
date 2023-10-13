553ccb72ad8129e041ba4243044474f9edb4f8fd
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2023-10-13 19:17:24 +0530

    updated at 7:17

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+import SearchBar from "@/components/SearchBar";
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="px-6  border-2 border-red-500 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text ">
+              Smart Shopping Start Here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className=" head-text">
+              Unleash the power of{" "}
+              <span className="text-primary"> PriceWise</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            <SearchBar />
+          </div>
+          HeroCarousel
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["Apple iPhone 15", "Fridge", "Sneakers", "AC"].map((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "Search" },
+  { src: "/assets/icons/black-heart.svg", alt: "Heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full ">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+          <p className="nav-logo">
+            Price <span className="text-primary">Wise</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
